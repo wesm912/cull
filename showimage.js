d6744aa70061049f27ebbc4b2550f1c9388c814e
Wesley Mitchell <wesm@studio.attlocal.net>
2025-06-10 19:08:27 -0700

    Simple test driver to make sure the cull window gets the stretched version of the passed image.

--- showimage.js
+++ showimage.js
@@ -1,12 +1,13 @@
 #include "AutoStretch.js"
+#include "PreviewWindow.js"
 
 function main() {
     console.show();
+    previewWindow = new PreviewWindow();
      var path = "/Volumes/G12T/Astro/starfront/SCA260/Capture/Owl Nebula/2025-05-18/Poseidon-M PRO/SCA260/2025-05-18/OIII/LIGHT_OIII_180.00s_1x1bin_deg_GAIN_125_OFFSET_20_-5.00C_0007.fits"
     var imageWindow =ImageWindow.open(path, "Preview"+Date.now(), "", true)[0];
     console.writeln("Image window: " + imageWindow + " is Null? " + imageWindow.isNull);
-    var autoStretch = new AutoStretch();
-    autoStretch.Apply(imageWindow.mainView);
-    imageWindow.show();
+    previewWindow.SetImage(imageWindow);
+    previewWindow.show();
 }
 main();
