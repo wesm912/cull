d2dec0725280a59b8c164737dce0c576be039a3b
Wesley Mitchell <wesm@studio.attlocal.net>
2025-06-11 16:09:39 -0700

    Logic to display the stretched window in a fixed size ImageWindow.

--- showimage.js
+++ showimage.js
@@ -1,3 +1,5 @@
+#include <pjsr/ResizeMode.jsh>
+//#include "AutoStretch.js"
 #include "PreviewWindow.js"
 
 function main() {
@@ -6,8 +8,15 @@ function main() {
      var path = "/Volumes/G12T/Astro/starfront/SCA260/Capture/Owl Nebula/2025-05-18/Poseidon-M PRO/SCA260/2025-05-18/OIII/LIGHT_OIII_180.00s_1x1bin_deg_GAIN_125_OFFSET_20_-5.00C_0007.fits"
     var imageWindow =ImageWindow.open(path, "Preview"+Date.now(), "", true)[0];
     console.writeln("Image window: " + imageWindow + " is Null? " + imageWindow.isNull);
+    let view = imageWindow.mainView, img = view.image;
+    let stf = new AutoStretch();
+    view.beginProcess();
+    img.resample(750, 500, ResizeMode_AbsolutePixels, AbsoluteResizeMode_ForceWidth);
+    stf.HardApply(view);
+    view.endProcess();
     previewWindow.SetImage(imageWindow);
+    imageWindow.show();
     imageWindow.forceClose();
-//    previewWindow.show();
+    //previewWindow.show();
 }
 main();
