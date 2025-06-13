79f10d0bbdfc5614378fe1586fee7a1fe748f51f
Wesley Mitchell <wesm@studio.attlocal.net>
2025-06-12 20:57:46 -0700

    Draw a reticle on a given image

---
+++ reticle.js
@@ -0,0 +1,53 @@
+#include <pjsr/ColorSpace.jsh>
+function Reticle()
+{
+    this.__base__ = Object;
+    this.__base__(  );
+
+    this.imageWindow = null;
+    this.view = null;
+    this.image = null;
+    
+    this.draw = function(win) {
+	if (win &&!win.isNull) {
+	    let view = win.mainView;
+	    if (view && view.image) {
+		let image = view.image;
+		let bmap = image.render();
+		let pen = new Pen(0xff008888, 1);
+		let graphics = new VectorGraphics(bmap);
+		let w = image.width, h = image.height,
+		    midX = w/2, midY = h/2;
+		try {
+		    graphics.pen = pen;
+		    graphics.drawLine(0, midY, w, midY);
+		    graphics.drawLine(midX, 0, midX, h);
+		    graphics.drawArc(midX, midY, 200, 0, 6.28);
+		} finally {
+		    graphics.end();
+		}
+		view.beginProcess();
+		image.blend(bmap);
+		view.endProcess();
+	    }
+	    win.show();
+	}
+    }
+};
+
+Reticle.prototype = new Object();
+
+//test
+function test()  {
+    var path = "/Volumes/G12T/Astro/starfront/SCA260/Capture/Owl Nebula/2025-05-18/Poseidon-M PRO/SCA260/2025-05-18/OIII/LIGHT_OIII_180.00s_1x1bin_deg_GAIN_125_OFFSET_20_-5.00C_0007.fits"
+    var imageWindow =ImageWindow.open(path, "Preview"+Date.now(), "", true)[0];
+    imageWindow.mainView.beginProcess();
+    imageWindow.mainView.image.colorSpace = ColorSpace_RGB;
+    imageWindow.mainView.endProcess();
+    reticle = new Reticle();
+		
+    reticle.draw(imageWindow);
+}
+#ifdef TEST_RETICLE
+test();
+#endif
